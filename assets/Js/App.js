3fb2b954066463d158392aca1de0a1ffdf92ba43
baran <rezvan.a78@gmail.com>
2022-08-25 22:25:09 +0330

    The name of my classes are chenged.

--- assets/Js/App.js
+++ assets/Js/App.js
@@ -1,35 +1,47 @@
 //begin p3main
-    function myFunction1() {
-      var x = document.getElementById("myDIV1");
+    function p3main_Js1() {
+      var x = document.getElementById("p3main-DIV1");
+      let y=document.getElementById("p3main-h3-1");
       if (x.style.display === "none") {
         x.style.display = "block";
+        x.style.color="#6e7780";
+        y.style.color="#004c6a";
       } else {
         x.style.display = "none";
       }
     }
 
-    function myFunction2() {
-        var x = document.getElementById("myDIV2");
+    function p3main_Js2() {
+        var x = document.getElementById("p3main-DIV2");
+        let y=document.getElementById("p3main-h3-2");
         if (x.style.display === "none") {
           x.style.display = "block";
+          x.style.color="#6e7780";
+          y.style.color="#004c6a";
         } else {
           x.style.display = "none";
         }
       }
 
-      function myFunction3() {
-        var x = document.getElementById("myDIV3");
+      function p3main_Js3() {
+        var x = document.getElementById("p3main-DIV3");
+        let y=document.getElementById("p3main-h3-3");
         if (x.style.display === "none") {
           x.style.display = "block";
+          x.style.color="#6e7780";
+          y.style.color="#004c6a";
         } else {
           x.style.display = "none";
         }
       }
 
-      function myFunction4() {
-        var x = document.getElementById("myDIV4");
+      function p3main_Js4() {
+        var x = document.getElementById("p3main-DIV4");
+        let y=document.getElementById("p3main-h3-4");
         if (x.style.display === "none") {
           x.style.display = "block";
+          x.style.color="#6e7780";
+          y.style.color="#004c6a";
         } else {
           x.style.display = "none";
         }
