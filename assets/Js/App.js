030da00baafc386872bf06fae6bd60ec25f0af13
PVZMF <mhmoghadamfar@gmail.com>
2022-08-30 21:43:56 +0430

    (Bug) => App.js p3main -> p3m

--- assets/Js/App.js
+++ assets/Js/App.js
@@ -12,8 +12,8 @@
     }
 
     function p3m_Js2() {
-        var x = document.getElementById("p3main-DIV2");
-        let y=document.getElementById("p3main-h3-2");
+        var x = document.getElementById("p3m-DIV2");
+        let y=document.getElementById("p3m-h3-2");
         if (x.style.display === "none") {
           x.style.display = "block";
           x.style.color="#6e7780";
@@ -24,8 +24,8 @@
       }
 
       function p3m_Js3() {
-        var x = document.getElementById("p3main-DIV3");
-        let y=document.getElementById("p3main-h3-3");
+        var x = document.getElementById("p3m-DIV3");
+        let y=document.getElementById("p3m-h3-3");
         if (x.style.display === "none") {
           x.style.display = "block";
           x.style.color="#6e7780";
