f7dd241fe862050607c96cf34775e1c3204a3cd7
PVZMF <mhmoghadamfar@gmail.com>
2022-09-01 21:09:45 +0430

    Fixed_som_bugs

--- assets/Js/App.js
+++ assets/Js/App.js
@@ -148,17 +148,17 @@ btnHamburger.addEventListener("click",collapse);
 const menulist = document.querySelector(".header__menulist");
 function collapse(){
   if(btnHamburger.classList.contains('header_btnhamburger-showmenu')){
+    document.querySelector('.header__btnEasyTrader').style.display = "flex";
     btnHamburger.classList.remove('header_btnhamburger-showmenu');
     menulist.classList.remove("header__menuMobile");
     document.querySelectorAll('.header__navitem').forEach(elem => elem.classList.remove('header_hideElement'));
     document.querySelector('.header__menuMobile_content').style.display = 'none';
     document.body.style.overflowY = "auto";
-    console.log("remove");
-    
   }else{
     btnHamburger.classList.add('header_btnhamburger-showmenu');
     menulist.classList.add("header__menuMobile");
     document.querySelectorAll('.header__navitem').forEach(elem => elem.classList.add('header_hideElement'));
+    document.querySelector('.header__btnEasyTrader').style.display = "none";
     document.querySelector('.header__menuMobile_content').style.display = 'flex';
     document.body.style.overflowY = "hidden";
   }
