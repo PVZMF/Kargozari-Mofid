9fd55f74735998aae0dcea418064d0462417e7ee
PVZMF <mhmoghadamfar@gmail.com>
2022-09-05 17:07:51 +0430

    Complete section p4m4 page about us

--- assets/Js/App4.js
+++ assets/Js/App4.js
@@ -45,11 +45,46 @@ if(showitem.classList.contains('header_hideElement')){
 
 
 // ****************************************************************************************************************** //
-/*Begin Header_page1 Moghaddamfa*/
+/*Begin p4m4 Moghaddamfar*/
 
 document.querySelectorAll(".p4m4__opt__item__btn").forEach((element,i) => element.addEventListener("click", ()=> aboutus(i)));
 
 function aboutus(item){
   document.querySelectorAll(".p4m4__opt__item__btn").forEach((element,i)=> i==item?element.classList.add("p4m4__opt__item__btn-active"):element.classList.remove("p4m4__opt__item__btn-active"));
-
+  switch(item){
+  case 0:
+    document.querySelector(".p4m4__opt__kargozariMofid").classList.remove("p4m4_hideelement");
+    if (!document.querySelector(".p4m4__opt__EntekhabMofid").classList.contains("p4m4_hideelement")) document.querySelector(".p4m4__opt__EntekhabMofid").classList.add("p4m4_hideelement");
+    if (!document.querySelector(".p4m4__opt__pardazeah").classList.contains("p4m4_hideelement")) document.querySelector(".p4m4__opt__pardazeah").classList.add("p4m4_hideelement");
+  
+    break;
+  case 1:
+    document.querySelector(".p4m4__opt__kargozariMofid").classList.add("p4m4_hideelement");
+    document.querySelector(".p4m4__opt__EntekhabMofid").classList.remove("p4m4_hideelement");
+    if (!document.querySelector(".p4m4__opt__pardazeah").classList.contains("p4m4_hideelement")) document.querySelector(".p4m4__opt__pardazeah").classList.add("p4m4_hideelement");
+    break;
+  case 2:
+    document.querySelector(".p4m4__opt__kargozariMofid").classList.add("p4m4_hideelement");
+    if (!document.querySelector(".p4m4__opt__EntekhabMofid").classList.contains("p4m4_hideelement")) document.querySelector(".p4m4__opt__EntekhabMofid").classList.add("p4m4_hideelement");
+    document.querySelector(".p4m4__opt__pardazeah").classList.remove("p4m4_hideelement");
+    break;
+  }
 }
+
+let allperson = document.querySelector(".p4m4__kargozariMofid__listmanagers");
+function addperson(srcimage,name,job){
+  let person = document.querySelector(".p4m4__kargozariMofid__oneperson");
+  let new_person = person.cloneNode(true);
+  allperson.appendChild(new_person);
+  document.querySelectorAll(".p4m4__kargozariMofid__image")[0].src = srcimage;
+  document.querySelectorAll(".p4m4__kargozarimofid__name")[0].innerHTML = name;
+  document.querySelectorAll(".p4m4__kargozariMofid__job")[0].innerHTML = job;
+}
+let srcimage = ["..//assets/Images/zamanifar.jpg","..//assets/Images/seifollahi.jpg","..//assets/Images/rsadeghi.jpg","..//assets/Images/r.vafaie.jpg"]
+let name = ["زمانی فر","صیف اللهی","صادقی","وفایی"];
+let job = ["مدیر ارشد دیجیتال","مدیر حساب مشتریان","مدیر بازاریابی","مدیر مالی"];
+for(let item=0;item<srcimage.length;item++){
+  addperson(srcimage[item], name[item],job[item]);
+}
+/*End p4m4 Moghaddamfar*/
+// ****************************************************************************************************************** //
